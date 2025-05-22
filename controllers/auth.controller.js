f0b5542351f9fa2630113d56db5b7fec00565c95
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-22 16:09:34 +0100

    Update auth.controller.js

---
+++ controllers/auth.controller.js
@@ -0,0 +1,58 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import asyncHandler from "express-async-handler";
+
+// Generate JWT
+const generateToken = (id, role) => {
+  return jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: "30d" });
+};
+
+// Register user
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, role, department } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+    department: role === "Admin" ? null : department,
+  });
+
+  res.status(201).json({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    department: user.department,
+    token: generateToken(user._id, user.role),
+  });
+});
+
+// Login user
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      department: user.department,
+      token: generateToken(user._id, user.role),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
+export { registerUser, loginUser };
