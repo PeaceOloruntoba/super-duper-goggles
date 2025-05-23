69ddd0b04000f3cd922ac0b2c4ec27137ad56b9b
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-23 11:39:52 +0100

    Update field.controller.js

---
+++ controllers/field.controller.js
@@ -0,0 +1,26 @@
+import asyncHandler from "express-async-handler";
+import MemoField from "../models/MemoField.js";
+
+// Create new memo field (Admin only)
+const createMemoField = asyncHandler(async (req, res) => {
+  const { name, type, required, options } = req.body;
+
+  const field = await MemoField.create({
+    name,
+    type,
+    required,
+    options: type === "select" ? options : [],
+    createdBy: req.user._id,
+  });
+
+  res.status(201).json(field);
+});
+
+// Get all memo fields
+const getMemoFields = asyncHandler(async (req, res) => {
+  const fields = await MemoField.find();
+  res.json(fields);
+});
+
+export { createMemoField, getMemoFields };
+ 
