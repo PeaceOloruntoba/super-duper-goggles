469608e46941012a01d2474a9fd1ced76bf80646
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-06-15 14:35:03 +0100

    qpp

--- models/User.js
+++ models/User.js
@@ -15,7 +15,6 @@ const userSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
 });
 
-// Hash password before saving
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   const salt = await bcrypt.genSalt(10);
@@ -23,7 +22,6 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-// Compare password for login
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
